61c25d099a00c292eb73a4a52acbf0f64b8d7575
YOONSUNGHYEON <qead753951@naver.com>
2020-12-23 14:49:03 +0900

    create spring project
    초기 spring 프로젝트 생성

---
+++ src/main/java/online_bookstore/OnlineBookstoreApplication.java
@@ -0,0 +1,13 @@
+package online_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online_bookstore/OnlineBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package online_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
