e6d367eea2cec7c9fce4c6699bcb550597de7677
LeeSeoYoung012 <lsyoung6540@gmail.com>
2021-01-23 03:50:44 +0900

    카트 가격 정보 출력

--- src/main/webapp/WEB-INF/views/member/cart.jsp
+++ src/main/webapp/WEB-INF/views/member/cart.jsp
@@ -5,8 +5,8 @@
 <head>
 <meta charset="UTF-8">
 <title>category list</title>
-<link href="${path}/resources/Member/css/cart.css" rel="stylesheet" type="text/css">
-	<script src="${path}/resources/cart/js/cart.js"></script>
+<link href="/resources/cart/css/cart.css" rel="stylesheet" type="text/css">
+	<script src="/resources/cart/js/cart.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <jsp:include page="../mainBase.jsp" />
@@ -17,7 +17,7 @@
 		<div class="col-xs-8 cartlist">
 			<div class="row carttop">
 				<span class="checkall">
-					<input type="checkbox" class="check-box"> 전체선택
+					<input type="checkbox" id="all_select" class="check-box"> 전체선택
 				</span>
 				<span class="checkallbtn">
 					<input type="button" class="cartbtn1" value="선택 위시리스트로 이동">
@@ -32,19 +32,10 @@
 		</div>
 		</div>
 		<div class="col-xs-3">
-			<div class="cartallpirce">
-				<div>1권을 선택하셨습니다.</div>
-				<div>
-					<span>총 상품금액</span>
-					<span class="price-right">7,000원</span>
-				</div>
-				<div>
-					<span>할인금액</span>
-					<span class="price-right">0원</span>
-				</div>
-				<div class="total-price">
-					<span>합계</span>
-					<span class="price-right total">7,000원</span>
+			<div id = cartallpirce_container>
+				<div class="cartallpirce">
+
+
 				</div>
 			</div>
 			<input type="button" class="buybutton" value="선택 구매하기" onclick="deleteSeveral();">
--- src/main/webapp/resources/cart/js/cart.js
+++ src/main/webapp/resources/cart/js/cart.js
@@ -1,15 +1,20 @@
 $(document).ready(function () {
 
     showCart();
-    $('cartbtn2').on('click', function(){
-        deleteFromcart()
-    })
+    $('#all_select').click(function(){
+        if($("input:checkbox[id='all_select']").prop("checked")){
+            $("input[type=checkbox]").prop("checked", true);
+        }else{
+            $("input[type=checkbox]").prop("checked", false);
+        }
+    });
 
 })
 
 
 function showCart(){
     let targetId = 1;
+    $('#booklist-container').empty();
     $.ajax({
         type: 'GET',
         url: `/api/cart/${targetId}`,
@@ -21,7 +26,9 @@ function showCart(){
                 let tempHtml = addcartItem( bookdto ,i);
                 $('#booklist-container').append(tempHtml);
             }
+            showtotalprice(response);
         }
+
     })
     console.log("cart.js done");
 }
@@ -30,15 +37,20 @@ function addHTML(){
 
 }
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function deleteFromcart(cartlistId){
     let memberNum = 1;
 
-    $('#booklist-container').empty(cartlistId);
+
     $.ajax({
         type: "DELETE",
         url: `/api/cart/${cartlistId}/${memberNum}`,
         success: function (response) {
-                showCart();
+             $('#booklist-container').empty(cartlistId);
+            showCart();
         }
     })
 }
@@ -68,13 +80,41 @@ function deleteSeveral(){
 
 }
 
-function deleteAll (){
-    $('#booklist-container').empty();
+function showtotalprice (response){
+
+    $('#cartallpirce_container').empty();
+    let totalprice=0;
+    for(let i=0; i<response.length; i++){
+        let bookdto = response[i];
+        totalprice += bookdto.book_Price;
+ }
+    let tempHtml = priceinfo(response.length,totalprice);
+    $('#cartallpirce_container').append(tempHtml);
+
+}
+
+function priceinfo(bookcount, totalprice){
+    return `<div class="cartallpirce"> 
+            <div>${bookcount}권을 선택하셨습니다.</div>
+            <div>
+                <span>총 상품금액</span>
+                <span class="price-right">${numberWithCommas(totalprice)}원</span>
+            </div>
+            <div>
+                <span>할인금액</span>
+                <span class="price-right">0원</span>
+            </div>
+                <div class="total-price">
+                <span>합계</span>
+                <span class="price-right total">${numberWithCommas(totalprice)}원</span>
+            </div>
+                </div>`;
 }
 
 function addcartItem(bookDTO,id){
 
-    return `<div class="row">
+    return `<div class = "cartbooklist">
+          <div class="row">
             <div class="col-xs-1"><input type="checkbox" name="rowcheckbox" id="${id}"></div>
             <div class="col-xs-2"><img style="width:60px; height:auto;" src=${bookDTO.book_Cover}>
             </div>
@@ -82,13 +122,14 @@ function addcartItem(bookDTO,id){
                 <div class="book-title">${bookDTO.book_Title}</div>
                 <div class="book-info">
                     <span class="book-author">${bookDTO.book_Author}</span>
-                    <span class="book-price">${bookDTO.book_Price}</span>
+                    <span class="book-price">${numberWithCommas(bookDTO.book_Price)}</span>
                 </div>
                 <div class="book-button">
                     <input type="button" id="${id}-move" class="cartbtn2" value="위시리스트로 이동">
                     <input type="button"  id="${id}-delete" class="cartbtn2" value="삭제" onclick="deleteFromcart('${id}')">
 
                 </div>
+            </div>
             </div>
         </div>`;
 }
